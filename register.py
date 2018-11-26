aa32f962b4a212e0df49278c8dffc1f156edd1ea
tedu <541432037@qq.com>
2018-11-26 19:37:22 +0800

    添加主UI界面

--- register.py
+++ register.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import QWidget,QApplication,QMainWindow,QMessageBox,QLabel
 # from Ui_register import *
 import Ui_register
 import Ui_login
+import Ui_main
 from PyQt5.QtCore import Qt,QRect
 import sys
 from socket import *
@@ -15,7 +16,10 @@ ADDR = (HOST,PORT)
 
 def varify_info(sockfd,username,password,mod):
     msg = mod+" %s %s"%(username,password)
-    sockfd.send(msg.encode())
+    try:
+        sockfd.send(msg.encode())
+    except OSError:
+        raise
     r = sockfd.recv(128).decode()
     return r
 
@@ -25,22 +29,29 @@ class Login_UI(Ui_login.Ui_MainWindow,QMainWindow):
         self.ui_control = ui_control
         self.setupUi(self)
         self.initUI()
-        
     def initUI(self):
         self.btnRegister.clicked.connect(self.toRegister)
         self.btnLogin.clicked.connect(self.doLogin)
     def doLogin(self):
         # 获取用户名和密码
-        username = self.lineUsername.text()
-        password = self.linePassword.text()
-        if not username or not password:
+        self.username = self.lineUsername.text()
+        self.password = self.linePassword.text()
+        if not self.username or not self.password:
             QMessageBox.about(self,'警告','请填写完整信息!')
             return
         sockfd = self.ui_control.sockfd
-        r = varify_info(sockfd,username,password,"L")#L代表登录
+        try:
+            r = varify_info(sockfd,self.username,self.password,"L")#L代表登录
+        except:
+            QMessageBox.about(self,'信息','暂时无法连接服务器!')
+            return
         if r == 'SUCCESS':
-            # 登录成功,进入网盘客户端界面
-            print("登录成功")
+            # 登录成功,初始化网盘客户端界面,隐藏登录界面
+            # 传递套接字和用户名
+            self.hide()
+            self.mainUI = Main_UI(self.ui_control.sockfd,self.username)
+            self.mainUI.show()
+
         elif r == 'FAIL':
             # 登录失败
             QMessageBox.about(self,'信息','用户名或密码错误!')
@@ -49,7 +60,6 @@ class Login_UI(Ui_login.Ui_MainWindow,QMainWindow):
     def toRegister(self):
         self.hide()
         self.ui_control.show()
-
 
 class Register_UI_Control(Ui_register.Ui_MainWindow,QMainWindow):
     def __init__(self):
@@ -116,6 +126,25 @@ class Register_UI_Control(Ui_register.Ui_MainWindow,QMainWindow):
             QMessageBox.about(self,'警告','服务器故障!')
             return
 
+class Main_UI(Ui_main.Ui_MainWindow,QMainWindow):
+    def __init__(self,sockfd,username):
+        super().__init__()
+        self.sockfd = sockfd
+        self.username = username
+        self.setupUi(self)
+        self.initUI()
+    def initUI(self):
+        # 显示用户信息至界面
+        self.laUsername.setText(self.username)
+        # 显示用户的文件列表
+        self.getFileList(self.sockfd)
+        # 绑定上传按钮功能
+        self.btnUpload.clicked.connect(self.uploadFile)
+
+    def getFileList(self,sockfd):
+        pass
+    def uploadFile(self):
+        pass
 
 
 if __name__ == "__main__":
